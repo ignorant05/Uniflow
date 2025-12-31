58b692a6bad69ea36c1586fafcf7a06002896b72
pebble <166765742+ignorant05@users.noreply.github.com>
2025-12-31 15:33:24 +0100

    Refactor: refactored github logs

--- platforms/github/logs/downloader.go
+++ platforms/github/logs/downloader.go
@@ -1,4 +1,4 @@
-package logs
+package github
 
 import (
 	"archive/zip"
@@ -67,9 +67,9 @@ func (s *Streamer) readLogs(logsUrl string) (string, error) {
 //
 // Example:
 // body, err := DownloadLogs(logsUrl)
-func DownloadLogs(logsUrl string) error {
+func DownloadLogs(logsUrl string) (string, error) {
 	if logsUrl == "" {
-		return fmt.Errorf("<?> Error: Invalid URL.\n")
+		return "", fmt.Errorf("<?> Error: Invalid URL.\n")
 	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
@@ -77,32 +77,32 @@ func DownloadLogs(logsUrl string) error {
 
 	req, err := http.NewRequestWithContext(ctx, "GET", logsUrl, nil)
 	if err != nil {
-		return fmt.Errorf("<?> Error: Create request: %w", err)
+		return "", fmt.Errorf("<?> Error: Create request: %w", err)
 	}
 	req.Header.Set("User-Agent", "Uniflow-CLI")
 
 	resp, err := DefaultClient.Do(req)
 	if err != nil {
-		return fmt.Errorf("<?> Error: Download: %w", err)
+		return "", fmt.Errorf("<?> Error: Download: %w", err)
 	}
 
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("<?> Error: Failed to download logs data.\n<?> Error: Status Code: %d\n", resp.StatusCode)
+		return "", fmt.Errorf("<?> Error: Failed to download logs data.\n<?> Error: Status Code: %d\n", resp.StatusCode)
 	}
 
 	data, err := io.ReadAll(io.LimitReader(resp.Body, constants.DATA_LOGS_MAX_SIZE))
 	if err != nil {
-		return fmt.Errorf("<?> Error: Failed to read logs data.\n<?> Error: %w\n", err)
+		return "", fmt.Errorf("<?> Error: Failed to read logs data.\n<?> Error: %w\n", err)
 	}
 
 	_, err = zip.NewReader(bytes.NewReader(data), int64(len(data)/1024))
 	if err != nil {
-		return fmt.Errorf("<?> Error: Failed to parse logs data.\n<?> Error: %w\n", err)
+		return "", fmt.Errorf("<?> Error: Failed to parse logs data.\n<?> Error: %w\n", err)
 	}
 
 	fmt.Printf("✓ Downloaded %d KB of logs\n\n", len(data)/1024)
 
-	return nil
+	return string(data), nil
 }
